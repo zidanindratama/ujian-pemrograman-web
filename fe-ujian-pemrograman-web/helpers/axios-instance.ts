ab39c8fadd9f22bb2953cc264389782d8b8d587b
zidanindratama <zidanindratama31@gmail.com>
2025-01-17 07:39:43 +0700

    fix: url api

--- fe-ujian-pemrograman-web/helpers/axios-instance.ts
+++ fe-ujian-pemrograman-web/helpers/axios-instance.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
+const DEV_API_URL = "http://localhost:3001";
+const PROD_API_URL = "https://be-ujian-pemrograman-web.vercel.app";
+
 const axiosInstance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development"
-      ? process.env.NEXT_PUBLIC_DEV_API_URL
-      : process.env.NEXT_PUBLIC_PROD_API_URL,
+  baseURL: process.env.NODE_ENV === "development" ? DEV_API_URL : PROD_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
